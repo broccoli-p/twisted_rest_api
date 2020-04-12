91f43fed74ecf2a50f8ce48f61925e1a9a7f57ad
Geontae park <iln1027@kakao.com>
2020-04-12 14:40:58 +0900

    2020-04-12|Geontae park|Xml을 이용한 URI 분배 로직 수정

--- req.py
+++ req.py
@@ -1,6 +1,6 @@
 import requests
-URL = 'http://127.0.0.1:9990/person'
+URL = 'http://localhost:9990/person'
 header = {'Context-Type':'application/json'}
 response = requests.post(URL, headers=header) 
-print response.status_code 
-print response.text
+print (response.status_code)
+print (response.content)
--- rest_server.py
+++ rest_server.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 #-*- encoding : UTF-8 -*-
+from __future__ import absolute_import, division, print_function
+
 from twisted.web import server, resource
 from twisted.internet import reactor, defer
 from importlib import import_module
@@ -15,37 +17,35 @@ def convertDictToJson(data):
 class DummyServer(resource.Resource):
 	isLeaf = True
 	def __init__(self, uriDict): 
-		# uriImport : 임포트 할 모듈명
-		# uriFunc : 임포트 할 함수명
-		self.uriName = uriDict[0]
-		self.uriImport = uriDict[1].split(".")[0] + "."
-		self.uriImport = self.uriImport + uriDict[1].split(".")[1]
-		self.uriFunc = uriDict[1].split(".")[2]
-
-		# m : 임포트된 모듈 핸들러
-		# func : 가져온 함수 핸들러
-		m = import_module(self.uriImport)
-		self.func = getattr(m, self.uriFunc)
+		self.uriDict = uriDict
 
 	def render_POST(self, request):
 		print("Received request for {0}".format(request.uri))
 		uri = request.uri.replace('/','')
-		context_type = request.getHeader('Context-Type')
+		contextType = request.getHeader('Context-Type')
 		ret = {'data': None, 'code':200}
-		if context_type != 'application/json':
+		if contextType != 'application/json':
 			ret['code'] = 400    
 		else:
 			ret['data'], ret['code'] = self._uriContext(uri)
         
-		request.setHeader('Content-Type', 'application/json"')
+		request.setHeader('Content-Type', 'application/json')
 		request.setResponseCode(ret['code'])
 		return convertDictToJson(ret)
 
 	def _uriContext(self, uri):
-		if uri in self.uriName:
-			# ret : 함수 결과값 불러오기
-			ret = self.func()
-			return {'err':'True', 'data':ret}, 200
+		if uri in self.uriDict:
+			mod = uriDict[uri].split('.')
+
+    		# uriImport : 임포트 할 모듈명
+			# uriFunc : 임포트 할 함수명
+			uriImport = '.'.join(mod[0:-1])
+			uriFunc = mod[-1]
+
+			# m : 임포트된 모듈 핸들러
+			# func : 가져온 함수 핸들러
+			m = import_module(uriImport)
+			return getattr(m, uriFunc)(), 200
 		else:
 			print("Immediate answering request for '/'")
 			return "Not Found", 404
@@ -54,12 +54,10 @@ class DummyServer(resource.Resource):
 def getUriMapping():
 	import xml.etree.ElementTree as ET
 
-	xml_tree = ET.parse(os.path.join(os.getcwd(), 'conf', 'url-mapping.xml'))
-	uriDict = xml_tree.getroot().findall('uri')
+	xmlTree = ET.parse(os.path.join(os.getcwd(), 'conf', 'url-mapping.xml'))
+	uriAttr = xmlTree.getroot().findall('uri')
 	func = lambda uri:map(uri.get, [k for k in uri.keys() if k=='name' or k =='func'])
-	for x in map(func, uriDict):
-		data = x
-	return data
+	return {data[0]:data[1] for data in map(func, uriAttr) if data != []}
 
 if __name__ == '__main__':
 	uriDict = getUriMapping()
